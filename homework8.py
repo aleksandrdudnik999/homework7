28a4c67cf82a06438cc642e7ceec59c5cb2b597c
aleksandrdudnik999 <168992506+aleksandrdudnik999@users.noreply.github.com>
2024-05-19 00:06:32 +0500

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'peach', 'lemon']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:5])
+my_list[3] = 'pear'
+print(my_list)
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин', 'kiwi': 'киви', 'peach': 'персик', 'lemon': 'лемон'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['banana'] = 'груша'
+print(my_dict)
