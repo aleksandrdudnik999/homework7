2fc1b84ffe147cf7cbc4627cca85c034e14abc14
aleksandrdudnik999 <168992506+aleksandrdudnik999@users.noreply.github.com>
2024-05-18 23:53:46 +0500

    Add files via upload

---
+++ module1hard.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(students)
+print(students)
+grades = [sum([5, 3, 3, 5, 4])/len([5, 3, 3, 5, 4]), sum([2, 2, 2, 3])/len([2, 2, 2, 3]), sum([4, 5, 5, 2])/len([4, 5, 5, 2]), sum([4, 4, 3])/len([4, 4, 3]), sum([5, 5, 5, 4, 5])/len([5, 5, 5, 4, 5])]
+students = {'Johnny': grades[0], 'Bilbo': grades[1], 'Steve': grades[2], 'Khendrik': grades[3], 'Aaron': grades[4]}
+print(students)
+students = {'Aaron': 4.8, 'Bilbo': 2.25, 'Johnny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.0}
+print(students)
